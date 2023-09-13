6bc7ead816b30fb796127dc0cba6dac1339eedd5
wros21 <34988061+wros21@users.noreply.github.com>
2023-09-13 16:41:26 -0600

    Changes and menu
    test

--- my-app/controllers/funciones_home.py
+++ my-app/controllers/funciones_home.py
@@ -178,8 +178,8 @@ def generarReporteExcel():
 
     hoja.append(cabeceraExcel)
 
-    # Formato para números en moneda colombiana y sin decimales
-    formato_moneda_colombiana = '#,##0'
+    # Formato para números en moneda Guatemalteca y sin decimales
+    formato_moneda_guatemalteco = '#,##0'
 
     # Agregar los registros a la hoja
     for registro in dataEmpleados:
@@ -374,6 +374,315 @@ def eliminarEmpleado(id_empleado, foto_empleado):
     except Exception as e:
         print(f"Error en eliminarEmpleado : {e}")
         return []
+
+
+######Proveedores#####
+
+ef procesar_form_proveedor(dataForm, foto_perfil):
+
+    result_foto_perfil = procesar_imagen_perfil(foto_perfil)
+    try:
+        with connectionBD() as conexion_MySQLdb:
+            with conexion_MySQLdb.cursor(dictionary=True) as cursor:
+
+                sql = "INSERT INTO tbl_proveedor (nombre_proveedor, apellido_proveedor, sexo_proveedor, telefono_proveedor, email_proveedor, profesion_proveedor, foto_proveedor) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+
+                # Creando una tupla con los valores del INSERT
+                valores = (dataForm['nombre_proveedor'], dataForm['apellido_proveedor'], dataForm['sexo_proveedor'],
+                           dataForm['telefono_proveedor'], dataForm['email_proveedor'], result_foto_perfil)
+                cursor.execute(sql, valores)
+
+                conexion_MySQLdb.commit()
+                resultado_insert = cursor.rowcount
+                return resultado_insert
+
+    except Exception as e:
+        return f'Se produjo un error en procesar_form_proveedor: {str(e)}'
+
+
+def procesar_imagen_perfil(foto):
+    try:
+        # Nombre original del archivo
+        filename = secure_filename(foto.filename)
+        extension = os.path.splitext(filename)[1]
+
+        # Creando un string de 50 caracteres
+        nuevoNameFile = (uuid.uuid4().hex + uuid.uuid4().hex)[:100]
+        nombreFile = nuevoNameFile + extension
+
+        # Construir la ruta completa de subida del archivo
+        basepath = os.path.abspath(os.path.dirname(__file__))
+        upload_dir = os.path.join(basepath, f'../static/fotos_proveedors/')
+
+        # Validar si existe la ruta y crearla si no existe
+        if not os.path.exists(upload_dir):
+            os.makedirs(upload_dir)
+            # Dando permiso a la carpeta
+            os.chmod(upload_dir, 0o755)
+
+        # Construir la ruta completa de subida del archivo
+        upload_path = os.path.join(upload_dir, nombreFile)
+        foto.save(upload_path)
+
+        return nombreFile
+
+    except Exception as e:
+        print("Error al procesar archivo:", e)
+        return []
+
+
+# Lista de proveedors
+def sql_lista_proveedorsBD():
+    try:
+        with connectionBD() as conexion_MySQLdb:
+            with conexion_MySQLdb.cursor(dictionary=True) as cursor:
+                querySQL = (f"""
+                    SELECT 
+                        e.id_proveedor,
+                        e.nombre_proveedor, 
+                        e.apellido_proveedor,
+                        e.foto_proveedor,
+                        CASE
+                            WHEN e.sexo_proveedor = 1 THEN 'Masculino'
+                            ELSE 'Femenino'
+                        END AS sexo_proveedor
+                    FROM tbl_proveedors AS e
+                    ORDER BY e.id_proveedor DESC
+                    """)
+                cursor.execute(querySQL,)
+                proveedorsBD = cursor.fetchall()
+        return proveedorsBD
+    except Exception as e:
+        print(
+            f"Errro en la función sql_lista_proveedorsBD: {e}")
+        return None
+
+
+# Detalles del proveedor
+def sql_detalles_proveedorsBD(idproveedor):
+    try:
+        with connectionBD() as conexion_MySQLdb:
+            with conexion_MySQLdb.cursor(dictionary=True) as cursor:
+                querySQL = ("""
+                    SELECT 
+                        e.id_proveedor,
+                        e.nombre_proveedor, 
+                        e.apellido_proveedor,
+                        CASE
+                            WHEN e.sexo_proveedor = 1 THEN 'Masculino'
+                            ELSE 'Femenino'
+                        END AS sexo_proveedor,
+                        e.telefono_proveedor, 
+                        e.email_proveedor,
+                        e.profesion_proveedor,
+                        e.foto_proveedor,
+                        DATE_FORMAT(e.fecha_registro, '%Y-%m-%d %h:%i %p') AS fecha_registro
+                    FROM tbl_proveedors AS e
+                    WHERE id_proveedor =%s
+                    ORDER BY e.id_proveedor DESC
+                    """)
+                cursor.execute(querySQL, (idproveedor,))
+                proveedorsBD = cursor.fetchone()
+        return proveedorsBD
+    except Exception as e:
+        print(
+            f"Errro en la función sql_detalles_proveedorsBD: {e}")
+        return None
+
+
+# Funcion proveedors Informe (Reporte)
+def proveedorsReporte():
+    try:
+        with connectionBD() as conexion_MySQLdb:
+            with conexion_MySQLdb.cursor(dictionary=True) as cursor:
+                querySQL = ("""
+                    SELECT 
+                        e.id_proveedor,
+                        e.nombre_proveedor, 
+                        e.apellido_proveedor,
+                        e.email_proveedor,
+                        e.telefono_proveedor,
+                        e.profesion_proveedor,
+                        DATE_FORMAT(e.fecha_registro, '%d de %b %Y %h:%i %p') AS fecha_registro,
+                        CASE
+                            WHEN e.sexo_proveedor = 1 THEN 'Masculino'
+                            ELSE 'Femenino'
+                        END AS sexo_proveedor
+                    FROM tbl_proveedors AS e
+                    ORDER BY e.id_proveedor DESC
+                    """)
+                cursor.execute(querySQL,)
+                proveedorsBD = cursor.fetchall()
+        return proveedorsBD
+    except Exception as e:
+        print(
+            f"Errro en la función proveedorsReporte: {e}")
+        return None
+
+
+def generarReporteExcel():
+    dataproveedors = proveedorsReporte()
+    wb = openpyxl.Workbook()
+    hoja = wb.active
+
+    # Agregar la fila de encabezado con los títulos
+    cabeceraExcel = ("Nombre", "Apellido", "Sexo",
+                     "Telefono", "Email", "Profesión", "Fecha de Ingreso")
+
+    hoja.append(cabeceraExcel)
+
+    # Formato para números en moneda Guatemalteca y sin decimales
+    formato_moneda_guatemalteco = '#,##0'
+
+    # Agregar los registros a la hoja
+    for registro in dataproveedors:
+        nombre_proveedor = registro['nombre_proveedor']
+        apellido_proveedor = registro['apellido_proveedor']
+        sexo_proveedor = registro['sexo_proveedor']
+        telefono_proveedor = registro['telefono_proveedor']
+        email_proveedor = registro['email_proveedor']
+        profesion_proveedor = registro['profesion_proveedor']
+        fecha_registro = registro['fecha_registro']
+
+        # Agregar los valores a la hoja
+        hoja.append((nombre_proveedor, apellido_proveedor, sexo_proveedor, telefono_proveedor, email_proveedor, profesion_proveedor,
+                    fecha_registro))
+
+        # Itera a través de las filas y aplica el formato a la columna G
+        for fila_num in range(2, hoja.max_row + 1):
+            columna = 7  # Columna G
+            celda = hoja.cell(row=fila_num, column=columna)
+            celda.number_format = formato_moneda_colombiana
+
+    fecha_actual = datetime.datetime.now()
+    archivoExcel = f"Reporte_proveedors_{fecha_actual.strftime('%Y_%m_%d')}.xlsx"
+    carpeta_descarga = "../static/downloads-excel"
+    ruta_descarga = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), carpeta_descarga)
+
+    if not os.path.exists(ruta_descarga):
+        os.makedirs(ruta_descarga)
+        # Dando permisos a la carpeta
+        os.chmod(ruta_descarga, 0o755)
+
+    ruta_archivo = os.path.join(ruta_descarga, archivoExcel)
+    wb.save(ruta_archivo)
+
+    # Enviar el archivo como respuesta HTTP
+    return send_file(ruta_archivo, as_attachment=True)
+
+
+def buscarproveedorBD(search):
+    try:
+        with connectionBD() as conexion_MySQLdb:
+            with conexion_MySQLdb.cursor(dictionary=True) as mycursor:
+                querySQL = ("""
+                        SELECT 
+                            e.id_proveedor,
+                            e.nombre_proveedor, 
+                            e.apellido_proveedor,
+                            CASE
+                                WHEN e.sexo_proveedor = 1 THEN 'Masculino'
+                                ELSE 'Femenino'
+                            END AS sexo_proveedor
+                        FROM tbl_proveedors AS e
+                        WHERE e.nombre_proveedor LIKE %s 
+                        ORDER BY e.id_proveedor DESC
+                    """)
+                search_pattern = f"%{search}%"  # Agregar "%" alrededor del término de búsqueda
+                mycursor.execute(querySQL, (search_pattern,))
+                resultado_busqueda = mycursor.fetchall()
+                return resultado_busqueda
+
+    except Exception as e:
+        print(f"Ocurrió un error en def buscarproveedorBD: {e}")
+        return []
+
+
+def buscarproveedorUnico(id):
+    try:
+        with connectionBD() as conexion_MySQLdb:
+            with conexion_MySQLdb.cursor(dictionary=True) as mycursor:
+                querySQL = ("""
+                        SELECT 
+                            e.id_proveedor,
+                            e.nombre_proveedor, 
+                            e.apellido_proveedor,
+                            e.sexo_proveedor,
+                            e.telefono_proveedor,
+                            e.email_proveedor,
+                            e.profesion_proveedor,
+                            e.foto_proveedor
+                        FROM tbl_proveedors AS e
+                        WHERE e.id_proveedor =%s LIMIT 1
+                    """)
+                mycursor.execute(querySQL, (id,))
+                proveedor = mycursor.fetchone()
+                return proveedor
+
+    except Exception as e:
+        print(f"Ocurrió un error en def buscarproveedorUnico: {e}")
+        return []
+
+
+def procesar_actualizacion_form(data):
+    try:
+        with connectionBD() as conexion_MySQLdb:
+            with conexion_MySQLdb.cursor(dictionary=True) as cursor:
+                nombre_proveedor = data.form['nombre_proveedor']
+                apellido_proveedor = data.form['apellido_proveedor']
+                sexo_proveedor = data.form['sexo_proveedor']
+                telefono_proveedor = data.form['telefono_proveedor']
+                email_proveedor = data.form['email_proveedor']
+                profesion_proveedor = data.form['profesion_proveedor']
+
+
+                if data.files['foto_proveedor']:
+                    file = data.files['foto_proveedor']
+                    fotoForm = procesar_imagen_perfil(file)
+
+                    querySQL = """
+                        UPDATE tbl_proveedors
+                        SET 
+                            nombre_proveedor = %s,
+                            apellido_proveedor = %s,
+                            sexo_proveedor = %s,
+                            telefono_proveedor = %s,
+                            email_proveedor = %s,
+                            profesion_proveedor = %s,
+                            foto_proveedor = %s
+                        WHERE id_proveedor = %s
+                    """
+                    values = (nombre_proveedor, apellido_proveedor, sexo_proveedor,
+                              telefono_proveedor, email_proveedor, profesion_proveedor,
+                            fotoForm, id_proveedor)
+                else:
+                    querySQL = """
+                        UPDATE tbl_proveedors
+                        SET 
+                            nombre_proveedor = %s,
+                            apellido_proveedor = %s,
+                            sexo_proveedor = %s,
+                            telefono_proveedor = %s,
+                            email_proveedor = %s,
+                            profesion_proveedor = %s,
+                        WHERE id_proveedor = %s
+                    """
+                    values = (nombre_proveedor, apellido_proveedor, sexo_proveedor,
+                              telefono_proveedor, email_proveedor, profesion_proveedor,
+                             id_proveedor)
+
+                cursor.execute(querySQL, values)
+                conexion_MySQLdb.commit()
+
+        return cursor.rowcount or []
+    except Exception as e:
+        print(f"Ocurrió un error en procesar_actualizacion_form: {e}")
+        return None
+
+
+
+###FIN PROVEEDORES####
 
 
 # Eliminar usuario
