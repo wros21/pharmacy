a2e71addef42abb8c52286146eb7a9622da27c9c
wros21 <34988061+wros21@users.noreply.github.com>
2023-09-13 15:14:39 -0600

    app
    Apply changes

---
+++ my-app/conexion/conexionBD.py
@@ -0,0 +1,25 @@
+
+
+# Importando Libreria mysql.connector para conectar Python con MySQL
+import mysql.connector
+
+
+def connectionBD():
+    try:
+        # connection = mysql.connector.connect(
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="pM0GwdDJs682t1jV",
+            database="pharmacy",
+            charset='utf8mb4',
+            collation='utf8mb4_unicode_ci',
+            raise_on_warnings=True
+
+        )
+        if connection.is_connected():
+            # print("Conexión exitosa a la BD")
+            return connection
+
+    except mysql.connector.Error as error:
+        print(f"No se pudo conectar: {error}")
